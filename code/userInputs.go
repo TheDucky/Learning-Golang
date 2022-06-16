e8e3de94085c5a7e3e4b1cbea1774f397d653d3f
TheDucky <dakshkuldeep12@gmail.com>
2022-06-16 20:55:05 +0530

    how to take user input?

---
+++ code/userInputs.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	var name string
+	var bday uint16
+	fmt.Print("What is your name? ")
+	fmt.Scan(&name)
+
+	fmt.Print("When were you born ", name + "? ")
+	fmt.Scan(&bday)
+
+	fmt.Printf("You are %d years old\n", 2022 - bday)
+
+	
+}
