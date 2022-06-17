503ea70f6d42d9bce87c613fbdfb529701e24d89
TheDucky <dakshkuldeep12@gmail.com>
2022-06-17 22:20:37 +0530

    removed those extra lines

--- code/userInputs.go
+++ code/userInputs.go
@@ -5,16 +5,15 @@ import (
 )
 
 func main() {
-	
+
 	var name string
 	var bday uint16
 	fmt.Print("What is your name? ")
 	fmt.Scan(&name)
 
-	fmt.Print("When were you born ", name + "? ")
+	fmt.Print("When were you born ", name+"? ")
 	fmt.Scan(&bday)
 
-	fmt.Printf("You are %d years old\n", 2022 - bday)
+	fmt.Printf("You are %d years old\n", 2022-bday)
 
-	
 }
